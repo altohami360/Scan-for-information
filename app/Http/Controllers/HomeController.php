2f2e31d71c71bd6f9591c3546714ad2c14ed1379
altohami360 <altohami360@gmail.com>
2022-07-18 02:29:07 +0300

    edit download controller thi tenth time

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,9 +10,6 @@ class HomeController extends Controller
 {
     public function home()
     {
-        if (Storage::disk('images')->exists('uploads\images\qrcode.png')) {
-            unlink(public_path('uploads\images\qrcode.png'));
-        }
         QrCode::margin(1)->format('png')->size(200)->generate(route('information', auth()->user()->id), public_path('uploads\images\qrcode.png'));
 
         return view('dashboard');
