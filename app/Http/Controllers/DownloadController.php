013adb1f8f4a4ba08c0c29e9951a6fda1d37df8d
altohami360 <altohami360@gmail.com>
2022-07-17 18:37:50 +0300

    error in downloadController fixed done

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Facades\File;
 
 class DownloadController extends Controller
 {
-    public function download(Request $request) {
-    
-    if(File::exists(public_path('uploads\images\qrcode.png'))){
-        unlink(public_path('uploads\images\qrcode.png'));
-    }
-    QrCode::margin(1)->format('png')->size(200)->generate(route('information', $request->id), public_path('uploads\images\qrcode.png'));
-    return response()->download(public_path('uploads\images\qrcode.png'));
+    public function download(Request $request)
+    {
 
-}
+        if (File::exists(public_path('uploads\images\qrcode.png'))) {
+            unlink(public_path('uploads\images\qrcode.png'));
+        }
+        QrCode::margin(1)->format('png')->size(200)->generate(route('information', auth()->user()->id), public_path('uploads\images\qrcode.png'));
+        return response()->download(public_path('uploads\images\qrcode.png'));
+    }
 }
