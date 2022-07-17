0c5bab17b6203ee4acb2dc2f2c03631642ce610c
altohami360 <altohami360@gmail.com>
2022-07-17 18:30:23 +0300

    some chane

---
+++ app/Http/Controllers/DownloadController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Illuminate\Support\Facades\File;
+
+class DownloadController extends Controller
+{
+    public function download(Request $request) {
+    
+    if(File::exists(public_path('uploads\images\qrcode.png'))){
+        unlink(public_path('uploads\images\qrcode.png'));
+    }
+    QrCode::margin(1)->format('png')->size(200)->generate(route('information', $request->id), public_path('uploads\images\qrcode.png'));
+    return response()->download(public_path('uploads\images\qrcode.png'));
+
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\DownloadController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\InformationController;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +21,9 @@ Route::get('/', function () {
     return redirect('/dashboard');
 });
 
+Route::get('download', [DownloadController::class, 'download'])->name('download');
+
 Route::get('information/{id}', [InformationController::class, 'show'])->name('information');
-
-
-Route::get('download', function () {
-    QrCode::margin(1)->format('png')->size(200)->generate(route('information', auth()->user()->id), public_path('uploads\images\qrcode.png'));
-    return response()->download(public_path('uploads\images\qrcode.png'));
-})->name('download');
 
 Route::get('/dashboard', [HomeController::class, 'home'])->middleware(['auth'])->name('dashboard');
 
