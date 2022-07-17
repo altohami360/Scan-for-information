cc7bdc31ea002ef1d666a749ad6c95c36cd5ea11
altohami360 <altohami360@gmail.com>
2022-07-17 18:56:29 +0300

    edit download controller

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -11,11 +11,15 @@ class DownloadController extends Controller
 {
     public function download(Request $request)
     {
+        $path = 'uploads\images';
+        $name = 'qrcode.png';
 
-        if (File::exists(public_path('uploads\images\qrcode.png'))) {
-            unlink(public_path('uploads\images\qrcode.png'));
+        if (File::exists(public_path($path))) {
+            unlink(public_path($path));
         }
-        QrCode::margin(1)->format('png')->size(200)->generate(route('information', auth()->user()->id), public_path('uploads\images\qrcode.png'));
-        return response()->download(public_path('uploads\images\qrcode.png'));
+
+        QrCode::margin(1)->format('png')->size(200)->generate(route('information', auth()->user()->id), public_path($path));
+        return response()->download(public_path($path), $name);
+
     }
 }
