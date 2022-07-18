ebadda77366e0e7513402afc7b6ea98c890152cf
altohami360 <altohami360@gmail.com>
2022-07-18 03:00:29 +0300

    edit download controller 12  time

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,13 +14,11 @@ class HomeController extends Controller
 
         $path = 'uploads\images\\' . auth()->user()->id . '.png';
 
-        if (!file_exists(public_path($path))) {
-            QrCode::margin(1)
-                ->format('png')
-                ->size(200)
-                ->generate(route('information', auth()->user()->id), public_path($path));
-        }
-        // return $path;
+        QrCode::margin(1)
+            ->format('png')
+            ->size(200)
+            ->generate(route('information', auth()->user()->id), public_path($path));
+
         return view('dashboard', compact('path'));
     }
 }
